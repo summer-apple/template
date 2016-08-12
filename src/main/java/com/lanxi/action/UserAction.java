1d5f13327a5b57e320f9aa83148c6a8a34b16fe7
xiadong <xiadong@188lanxi.com>
2016-08-12 10:01:31 +0800

    Initial commit

---
+++ src/main/java/com/lanxi/action/UserAction.java
@@ -0,0 +1,18 @@
+package com.lanxi.action;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by summer on 16-8-11.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserAction {
+
+
+
+
+
+
+}
