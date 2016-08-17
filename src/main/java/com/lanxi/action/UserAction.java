513386d7b6a8da6627a36b5b53580cf8f346b56a
summer <summer.okay@gmail.com>
2016-08-17 23:09:23 +0800

    finish ssm version 1

--- src/main/java/com/lanxi/action/UserAction.java
+++ src/main/java/com/lanxi/action/UserAction.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
-public class UserAction {
+public class    UserAction {
 
     @Autowired
     UserService userService;
--- src/main/java/com/lanxi/dao/UserMapper.java
+++ src/main/java/com/lanxi/dao/UserMapper.java
@@ -1,11 +1,6 @@
 package com.lanxi.dao;
 
 import com.lanxi.entity.User;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
